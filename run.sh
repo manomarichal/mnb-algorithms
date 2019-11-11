844026d8eddadb95c92a369f1aa79f07cb0b9d51
sharogue <mano.marichal@student.uantwerpen.be>
2019-11-11 22:07:08 +0100

    compile

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-./indiv cyk input-cyk1.json baaba output-cyk1.html
-./indiv cyk input-cyk1.json abba output-cyk1.html
-./indiv cyk input-cyk2.json aaabb output-cyk2.html
-./indiv cyk input-cyk2.json aaabbb output-cyk2.html
+./indiv cyk input-cyk1.json baaba output-cyk1-1.html
+./indiv cyk input-cyk1.json abba output-cyk1-2.html
+./indiv cyk input-cyk2.json aabb output-cyk2-1.html
+./indiv cyk input-cyk2.json abbb output-cyk2-2.html
 
 ./indiv pda2cfg input-pda2cfg1.json output-pda2cfg1.txt
 ./indiv pda2cfg input-pda2cfg2.json output-pda2cfg2.txt
